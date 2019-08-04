1135397fbee9cdf6adf9e1d4ecf1cb1c7440a1aa
halcyoona <halcyoona@gmail.com>
2019-08-05 02:12:46 +0500

    redux with react

---
+++ my-redux/redux-example/src/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+import {Provider} from "react-redux";
+import {createStore , applyMiddleware} from "redux";
+import store from './store'
+// const store = createStore(() => [], {}, applyMiddleware());
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React with Redux</h1>
+          </header>
+          
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
