f5070d771a11765a1c62a4a42c51640ce38e65f7
halcyoona <halcyoona@gmail.com>
2020-04-22 19:27:05 +0500

    source

---
+++ gatsby_site/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Link from 'gatsby-link'
+import Counter from "./counter"
+
+export default () => 
+        <div>
+            <h1>Hello world! hello</h1>
+            <Link to="/page-2/">Page-2</Link>
+            <br/>
+            <Link to="/dir1/page-3/">Page-3</Link>
+            <br/>
+            <Link to="/counter/">Counter</Link>
+        </div>
+                    
